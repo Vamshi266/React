22b974eab2dfda37f0440c3d3ba28ed77a31b794
vamshigudimalla <vamshigudimalla38@gmail.com>
2021-01-15 11:26:38 +0530

    dynamic-values in react

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+
+  const title = "welcome to dojo-blog";
+  const likes = "50"; //what ever the data recat gonna convert 
+  // it to string while displaying to browser
+  // we cannot display "object" and "boolean" data on to browser
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className="content">
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        <h2>{10}</h2> {/* we can add dynamic content directly in {}*/}
+        <p>{[1,2,3,4,5]}</p>
+        <p>{Math.random() * 20}</p>
+       </div>
     </div>
   );
 }
